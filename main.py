90b7ee23884933a429a3d3150548da020cd2e9aa
babatakahiro1983 <32314308+babatakahiro1983@users.noreply.github.com>
2017-12-28 16:33:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from moviepy.editor import VideoFileClip
+from object_detection import ObjectDetector
 from lane_finder import LaneFinder
 from object_detect_yolo import YoloDetector
-
 
 if __name__ == "__main__":
     def remove_mp4_extension(file_name):
         return file_name.replace(".mp4", "")
 
-    yolo = YoloDetector()
-    lane_finder = LaneFinder(save_original_images=True, object_detection_func=yolo.process_image_array)
+    # Read movie
+    # yolo = YoloDetector()
+    # lane_finder = LaneFinder(save_original_images=True, object_detection_func=yolo.process_image_array)
+    object_detection = ObjectDetector()
+
     video_file = 'project_video.mp4'
-    # video_file = 'challenge_video.mp4'
-    # video_file = 'back_home.mov'
-    # video_file = 'file01_2017322191247.mp4'
     clip = VideoFileClip(video_file, audio=False)
-    t_start = 0
-    t_end = 0
+    t_start = 10
+    t_end = 15
     if t_end > 0.0:
         clip = clip.subclip(t_start=t_start, t_end=t_end)
     else:
         clip = clip.subclip(t_start=t_start)
 
-    clip = clip.fl_image(lane_finder.process_image)
+    clip = clip.fl_image(object_detection.process_image)
     clip.write_videofile("{}_output.mp4".format(remove_mp4_extension(video_file)), audio=False)
-    yolo.shutdown()
+
+
+
+
+
+
+
 
